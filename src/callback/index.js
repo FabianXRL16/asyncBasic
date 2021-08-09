d4920b3ad416d9048b7aa59635add1c459ab2343
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-09 07:19:30 -0500

    First callback

--- src/callback/index.js
+++ src/callback/index.js
@@ -1,2 +1,24 @@
+function sum(num1, num2){
+    return num1 + num2
+}
+
+function sumAsync(num1, num2, callback){
+    console.log(callback(num1, num2))
+}
+
+sumAsync(8, 10, sum)
 
 
+function date(callback){
+    console.log(new Date())
+    setTimeout(function (){
+        let date = new Date
+        callback(date)
+    }, 3000)
+}
+
+function printDate(dateNow){
+    console.log(dateNow)
+}
+
+date(printDate)
