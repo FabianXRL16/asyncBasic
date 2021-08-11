03685a4b7db064b8768d6f4963ad4810cb34b056
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-11 13:29:09 -0500

    challenge promises

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,17 +1,17 @@
 let XMLHttpRequest = require("XMLHttpRequest").XMLHttpRequest;
 
-module.exports = function fetchData(url_api){
-    return new Promise = ((resolve, reject) => {
+module.exports = fetchData = (url_api) => {
+    return new Promise ((resolve, reject) => {
         const xhttp = new XMLHttpRequest();
         xhttp.open("GET", url_api, true);
         xhttp.onreadystatechange = (() => {
             if(xhttp.readyState === 4){
-                (xhttp.readyState === 4)
-                    ? resolve(JSON.parse(resolve.responseText))
-                    : reject(new Error(`Error: ${reject} ${url_api}`))
+                (xhttp.status === 200)
+                    ? resolve(JSON.parse(xhttp.responseText))
+                    : reject(new Error(`Error ${url_api}`))
             }
-            xhttp.send();
         });
+        xhttp.send();
     });
 }
 
